2462f1d2c3a7f4b8c53829eba808cd08ad399559
Ramazan Ediz Değirmenci <ediz@192.168.1.113>
2023-11-17 02:24:37 +0300

    [dev] sanctum authentication login-logout

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response([
+                'message' => ['The provided credentials are incorrect.']
+            ], 500);
+        }
+
+        $userToken = $user->createToken('api-token')->plainTextToken;
+
+        return response(['token' => $userToken], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response(['message' => 'Logged out'], 200);
+    }
+    
+}
